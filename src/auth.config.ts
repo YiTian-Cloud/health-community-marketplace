a5e014be94e02c230a71555fac8aeb93c6ca6c00
Yi Tian <ytian98@yahoo.com>
2025-12-17 22:24:39 -0800

    Test debug

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -2,14 +2,11 @@
 import type { NextAuthConfig } from "next-auth";
 import GitHub from "next-auth/providers/github";
 
-console.log("LOCAL AUTH_URL", process.env.AUTH_URL);
-console.log("LOCAL GITHUB_ID set?", !!process.env.GITHUB_ID);
-console.log("LOCAL GITHUB_SECRET set?", !!process.env.GITHUB_SECRET);
-console.log("LOCAL AUTH_SECRET set?", !!process.env.AUTH_SECRET);
 
 export const authConfig: NextAuthConfig = {
   secret: process.env.AUTH_SECRET,
   trustHost: true,
+  debug: true,
   pages: { signIn: "/login" },
   providers: [
     GitHub({
